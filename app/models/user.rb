d96b75d2f78c2de0ed28ff7663f7dc65854cfa53
ariefrahman <arief.rahman279@yahoo.com>
2017-04-25 18:01:17 +0700

    create basic rspec for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   validates :username, :email, presence: true
   validates :email, uniqueness: true
+  validates :password, length: { in: 6..20 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  api_version(:module => "Api::V1", :header => {:name => "Accept", :value => "application/vnd.gadup.com; version=1"}, :path => {:value => "v1"}, :defaults => {:format => :json}, :default => true) do
+
+  api_version(:module => "Api::V1", :header => {:name => "Accept", :value => "application/vnd.gadup.com; version=1"}, :path => {:value => "v1"}, :defaults => {:format => :json} ) do
     resources :users, only: :show
   end
 end
