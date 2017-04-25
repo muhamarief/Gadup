1130a4651ce4e4024b056c34ea871be867e1f90b
ariefrahman <arief.rahman279@yahoo.com>
2017-04-25 12:51:34 +0700

    use versionist for restful api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  api_version(:module => "V1", :path => {:value => "v1"}, :defaults => {:format => "json"}) do
+  end
   api_version(:module => "Api::V1", :header => {:name => "Accept", :value => "application/vnd.gadup.com; version=1"}, :path => {:value => "v1"}, :defaults => {:format => :json}, :default => true) do
-    resources :users, :only => :index
+    resources :users
+    resources :admins, :only => [:index]
   end
 end
