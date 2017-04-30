9aaaa6f7ae3b8db5cba6aba4db06368e537b10ba
ariefrahman <arief.rahman279@yahoo.com>
2017-04-30 11:05:18 +0700

    refactor api key rspec

--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -2,14 +2,19 @@ require 'rails_helper'
 
 RSpec.describe ApiKey, type: :model do
 
-  let (:owner) { FactoryGirl.create(:user) }
-  let (:api_key) { owner.api_keys.create }
+  let (:api_key) { create(:user_api_key ) }
+  # let (:api_key) { FactoryGirl.create(:user_api_key) }
 
-  subject { api_key}
+
+  subject { api_key }
 
   it { is_expected.to respond_to(:access_token) }
   it { is_expected.to respond_to(:owner) }
 
-  it { is_expected.to validate_presence_of(:access_token) }
+  it { is_expected.to validate_uniqueness_of(:access_token) }
   it { is_expected.to validate_presence_of(:owner) }
+
+  it "generate access token every creation" do
+    expect(api_key.access_token).to_not be_nil
+  end
 end
