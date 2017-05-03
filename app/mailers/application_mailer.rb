63a5df9a01ff92da4ad567ca2f834a9a0d5cdf28
ariefrahman <arief.rahman279@yahoo.com>
2017-05-03 18:50:29 +0700

    create user confirmable by sending email confirmation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,17 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  #default email development
+  default from: 'marief.rahman279@gmail.com'
   layout 'mailer'
+
+  # #default email production
+  # default from: "Gadup Empower Your Life <hello@gadup.co.id>", reply_to: "Gadup Support<support@gadup.co.id>"
+  # layout 'mailer'
+
+  protected
+  def format_recipient(recipient)
+    if recipient.is_a?(User)
+      %("#{recipient.username}" <#{recipient.email}>)
+    end
+  end
+  
 end
