d411057e05544c18fd96716fc3f401bf57a89db9
ariefrahman <arief.rahman279@yahoo.com>
2017-05-03 18:50:29 +0700

    create user confirmable by sending email confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,10 +8,28 @@ class Api::V1::UsersController < Api::V1::BaseController
   def create
     @user = User.new(user_params)
     if @user.save
-      api_key = @user.api_keys.create!
-      render json: { access_token: api_key.access_token }, status: :ok
+      UserMailer.welcome_email(@user).deliver_now
+      render json: { success: "Please confirm your email address to continue" }
     else
       render json: { error: "Email has been taken" }
+    end
+  end
+
+  def confirm_email
+    user = User.find_by(confirmation_token: params[:id])
+    if user
+      if user.confirmation_expired?
+        user.after_expired_confirmation_email
+        UserMailer.welcome_email(@user).deliver_now
+        render json: { success: "Please confirm your email address to continue" }
+      else
+        user.activate_account
+        @user = user
+        api_key = @user.api_keys.create!
+        render json: { access_token: api_key.access_token }, status: :ok
+      end
+    else
+      render json: { error: "Sorry. User does not exist" }
     end
   end
 
@@ -19,7 +37,5 @@ class Api::V1::UsersController < Api::V1::BaseController
   def user_params
     permit(:email, :password, :password_confirmation, :username)
   end
-
-
 
 end
