573a2205ed87b958cfa945da4dc527efad65c97b
ariefrahman <arief.rahman279@yahoo.com>
2017-05-04 09:14:50 +0700

    add delayed_job gem and implement for user confirmation email

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.welcome_email(@user).deliver_now
+      UserMailer.welcome_email(@user).deliver_later
       render json: { success: "Please confirm your email address to continue" }
     else
       render json: { error: "Email has been taken" }
@@ -20,7 +20,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     if user
       if user.confirmation_expired?
         user.after_expired_confirmation_email
-        UserMailer.welcome_email(@user).deliver_now
+        UserMailer.welcome_email(@user).deliver_later
         render json: { success: "Please confirm your email address to continue" }
       else
         user.activate_account
