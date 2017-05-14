dbde3c13de12b013204937b1e6e8ac285b5f9c6d
ariefrahman <arief.rahman279@yahoo.com>
2017-05-14 13:25:16 +0700

    create user headers view for mobile and desktop

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,12 +1,17 @@
 module Authenticable
   extend ActiveSupport::Concern
 
-  def current_api_key
-    @current_api_key ||= ApiKey.includes(:owner).find_by(access_token: access_token)
-  end
+  ## Rails Api Based Authenticable
+  # def current_api_key
+  #   @current_api_key ||= ApiKey.includes(:owner).find_by(access_token: access_token)
+  # end
+  #
+  # def current_authenticatee
+  #   return current_api_key.owner unless current_api_key.nil?
+  # end
 
+  # Rails session based Authentiacble
   def current_authenticatee
-    return current_api_key.owner unless current_api_key.nil?
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
 end
--- app/views/api/v1/users/_user.json.jbuilder
+++ app/views/api/v1/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.(user, :id, :username, :email)
+json.(user, :id, :full_name, :first_name, :last_name, :email)
 json.(:created_at, :updated_at) unless user.id == nil
