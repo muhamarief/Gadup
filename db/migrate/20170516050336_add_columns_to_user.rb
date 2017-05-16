db4c1c9c38b01d098c5ae248914e77cbca8baeef
ariefrahman <arief.rahman279@yahoo.com>
2017-05-16 15:54:31 +0700

    create user edit page before use platform and update controller

---
+++ db/migrate/20170516050336_add_columns_to_user.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :birthday, :date
+    add_column :users, :gender, :integer
+    add_column :users, :city, :string
+    add_column :users, :profile_picture, :string
+  end
+end
