cf7ad2b4c10470019040f7e324c07245ffb0f5b4
ariefrahman <arief.rahman279@yahoo.com>
2017-05-18 17:10:28 +0700

    change spending-income date&time to be datetime format

--- db/migrate/20170514124655_create_spendings.rb
+++ db/migrate/20170514124655_create_spendings.rb
@@ -2,8 +2,7 @@ class CreateSpendings < ActiveRecord::Migration[5.0]
   def change
     create_table :spendings do |t|
       t.float :nominal, null: false
-      t.date :spending_date, null: false
-      t.time :spending_time, null: false
+      t.datetime :spending_time, null: false
       t.references :wallet, null: false
 
       t.timestamps
--- db/migrate/20170514124700_create_incomes.rb
+++ db/migrate/20170514124700_create_incomes.rb
@@ -2,8 +2,7 @@ class CreateIncomes < ActiveRecord::Migration[5.0]
   def change
     create_table :incomes do |t|
       t.float :nominal, null: false
-      t.date :transaction_date, null: false
-      t.time :transaction_time, null: false
+      t.datetime :transaction_time, null: false
       t.references :wallet, null: false
 
       t.timestamps
