f71929c5f087e2353449445d6f066d643bdb8987
ariefrahman <arief.rahman279@yahoo.com>
2017-05-27 09:37:52 +0700

    add feeds and entries for admin with view

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,5 @@
+$(document).on('turbolinks:load', function() {
+  $('#datetimepicker3').datetimepicker({
+    format: 'YYYY-MM-DD HH:mm'
+  });
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(first_name: 'Muhamad Arief', last_name: Rahman', email: 'arief.rahman279@yahoo.com', password: '123123123')
+create_user = User.create(first_name: "Arief", last_name: "Rahman", full_name: 'Arief Rahman', email: "arief_coolz@yahoo.com", password: '123123123', password_confirmation: '123123123', email_confirmed: true, birthday: "1995-09-27", gender: 1, city: "Jakarta", phone_number: "0818395967")
+
+create_admin = Admin.create(name: "Administrator", email: "admin@gadup.co.id", password: "gadup123", password_confirmation: "gadup123", super_admin: true)
