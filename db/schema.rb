b9e1f9e437611191298a1a909d24c41978b4eef1
ariefrahman <arief.rahman279@yahoo.com>
2017-07-14 11:01:39 +0700

    add wallet setting for wallet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523075033) do
+ActiveRecord::Schema.define(version: 20170713094634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,14 @@ ActiveRecord::Schema.define(version: 20170523075033) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "wallet_settings", force: :cascade do |t|
+    t.integer  "save_plan"
+    t.integer  "wallets_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["wallets_id"], name: "index_wallet_settings_on_wallets_id", unique: true, using: :btree
+  end
+
   create_table "wallets", force: :cascade do |t|
     t.string   "owner_type"
     t.integer  "owner_id",                     null: false
@@ -150,4 +158,5 @@ ActiveRecord::Schema.define(version: 20170523075033) do
     t.index ["owner_type", "owner_id"], name: "index_wallets_on_owner_type_and_owner_id", using: :btree
   end
 
+  add_foreign_key "wallet_settings", "wallets", column: "wallets_id"
 end
